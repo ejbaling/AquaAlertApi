84f9261c86ef8a8b270281311411466c77aa0cc3
Emmanuel Jomer Baling <ej.baling@gmail.com>
2025-02-22 10:59:19 +1300

    Disable logging for mqtt client

--- AquaAlertApi/Services/MqttClientService.cs
+++ AquaAlertApi/Services/MqttClientService.cs
@@ -5,70 +5,67 @@ using System.Text;
 namespace AquaAlertApi.Services.MqttClientService
 {
     public class MqttClientService : IHostedService
-{
-    private readonly ILogger<MqttClientService> _logger;
-
-    public MqttClientService(ILogger<MqttClientService> logger)
     {
-        _logger = logger;
-    }
+        private readonly ILogger<MqttClientService> _logger;
+        private readonly IConfiguration _configuration;
 
-    public async Task StartAsync(CancellationToken cancellationToken)
-    {
-        /*
-         * This sample creates a simple MQTT client and connects to a public broker.
-         *
-         * Always dispose the client when it is no longer used.
-         * The default version of MQTT is 3.1.1.
-         */
-
-        var mqttFactory = new MqttClientFactory();
-
-        var mqttClient = mqttFactory.CreateMqttClient();
-        var mqttClientOptions = new MqttClientOptionsBuilder()
-            .WithTcpServer("10.0.0.120", 1883)
-            .WithClientId("AquaAlertApiClient")
-            .WithCredentials("mqtt-user", "Ng7tov!KhVv3")
-            .Build();
-
-        // Setup message handling before connecting so that queued messages
-        // are also handled properly. When there is no event handler attached all
-        // received messages get lost.
-        mqttClient.ApplicationMessageReceivedAsync += e =>
+        public MqttClientService(ILogger<MqttClientService> logger, IConfiguration configuration)
         {
-            _logger.LogInformation("Received mesage: {Parameter} cm", Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
-            return Task.CompletedTask;
-        };
+            _logger = logger;
+            _configuration = configuration;
+        }
 
-        // This will throw an exception if the server is not available.
-        // The result from this message returns additional data which was sent
-        // from the server. Please refer to the MQTT protocol specification for details.
-        await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
-        _logger.LogInformation("The MQTT client is connected.");
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var mqttFactory = new MqttClientFactory();
 
-        // Subscribe to a topic
-        var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder().WithTopicFilter("/sh/water-distance").Build();
-        await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
-        _logger.LogInformation("MQTT client subscribed to topic.");
+            var mqttClient = mqttFactory.CreateMqttClient();
+            var brokerIp = _configuration["MqttBroker:Ip"];
+            var brokerPort = _configuration["MqttBroker:Port"];
+
+            if (string.IsNullOrWhiteSpace(brokerIp) || string.IsNullOrWhiteSpace(brokerPort))
+            {
+                _logger.LogError("MQTT broker IP or port is not configured.");
+                return;
+            }
+
+            var mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer(brokerIp, int.Parse(brokerPort))
+                .WithClientId("AquaAlertApiClient")
+                .WithCredentials("mqtt-user", "Ng7tov!KhVv3")
+                .Build();
+
+            mqttClient.ApplicationMessageReceivedAsync += e =>
+            {
+                _logger.LogInformation("Received mesage: {Parameter} cm", Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
+                return Task.CompletedTask;
+            };
+
+            await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+            _logger.LogInformation("The MQTT client is connected.");
+
+            var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder().WithTopicFilter("/sh/water-distance").Build();
+            await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+            _logger.LogInformation("MQTT client subscribed to topic.");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            /*
+            * This sample disconnects from the server with sending a DISCONNECT packet.
+            * This way of disconnecting is treated as a clean disconnect which will not
+            * trigger sending the last will etc.
+            */
+
+            var mqttFactory = new MqttClientFactory();
+            var mqttClient = mqttFactory.CreateMqttClient();
+
+            // Send a clean disconnect to the server by calling _DisconnectAsync_. Without this the TCP connection
+            // gets dropped and the server will handle this as a non clean disconnect (see MQTT spec for details).
+            var mqttClientDisconnectOptions = mqttFactory.CreateClientDisconnectOptionsBuilder().Build();
+            await mqttClient.DisconnectAsync(mqttClientDisconnectOptions, CancellationToken.None);
+
+            _logger.LogInformation("The MQTT client is disconnected.");
+        }
     }
-
-    public async Task StopAsync(CancellationToken cancellationToken)
-    {
-        /*
-        * This sample disconnects from the server with sending a DISCONNECT packet.
-        * This way of disconnecting is treated as a clean disconnect which will not
-        * trigger sending the last will etc.
-        */
-
-        var mqttFactory = new MqttClientFactory();
-        var mqttClient = mqttFactory.CreateMqttClient();
-
-        // Send a clean disconnect to the server by calling _DisconnectAsync_. Without this the TCP connection
-        // gets dropped and the server will handle this as a non clean disconnect (see MQTT spec for details).
-        var mqttClientDisconnectOptions = mqttFactory.CreateClientDisconnectOptionsBuilder().Build();
-        await mqttClient.DisconnectAsync(mqttClientDisconnectOptions, CancellationToken.None);
-
-        _logger.LogInformation("The MQTT client is disconnected.");
-    }
-}
 }

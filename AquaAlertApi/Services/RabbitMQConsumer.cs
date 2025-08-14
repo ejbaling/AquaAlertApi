0b9d0df84f66973a39dbf7ce0a3b58aa96997505
Emmanuel Jomer Baling <ej.baling@gmail.com>
2025-08-14 19:24:28 +1200

    Install and use LaunchDarkly

--- AquaAlertApi/Services/RabbitMQConsumer.cs
+++ AquaAlertApi/Services/RabbitMQConsumer.cs
@@ -1,6 +1,8 @@
 using AquaAlertApi.Contracts;
 using AquaAlertApi.Data;
 using MassTransit;
+using LaunchDarkly.Sdk;
+using LaunchDarkly.Sdk.Client;
 
 namespace AquaAlertApi.Services
 {
@@ -11,6 +13,7 @@ namespace AquaAlertApi.Services
         private readonly IConfiguration _configuration;
         private static int _messageCount = 0;
         private readonly int LogEveryNMessages = 60; // every 5 minutes if messages are sent every 5 seconds. This can be adjusted via configuration.
+        private readonly LdClient _ldClient;
 
         public RabbitMQConsumer(ILogger<RabbitMQConsumer> logger, IConfiguration configuration, AppDbContext db)
         {
@@ -18,6 +21,20 @@ namespace AquaAlertApi.Services
             _configuration = configuration;
             LogEveryNMessages = configuration.GetValue<int>("RabbitMQ:LogEveryNMessages", 60);
             _db = db;
+
+            // Initialize LaunchDarkly client
+            var context = Context.New("context-key-123abc");
+            var timeSpan = TimeSpan.FromSeconds(10);
+            _ldClient = LdClient.Init(
+                Configuration.Default("mob-010039af-0db7-4729-9e83-e02c0d53cf6d", LaunchDarkly.Sdk.Client.ConfigurationBuilder.AutoEnvAttributes.Enabled),
+                context,
+                timeSpan
+            );
+
+            if (_ldClient.Initialized)
+                _logger.LogInformation("LaunchDarkly client successfully initialized!");
+            else
+                _logger.LogInformation("LaunchDarkly client failed to initialize");
         }
 
         public async Task Consume(ConsumeContext<MqttMessage> context)
@@ -36,12 +53,20 @@ namespace AquaAlertApi.Services
                 };
 
                 await _db.WaterLevelLogs.AddAsync(log);
-                await _db.SaveChangesAsync();
+
+                var flagValue = _ldClient.BoolVariation("save-to-database-feature", false);
+                if (flagValue)
+                    await _db.SaveChangesAsync();
 
                 _logger.LogInformation("Water level log created with ID: {Id}", log.Id);
 
-                 Interlocked.Exchange(ref _messageCount, 0);
+                Interlocked.Exchange(ref _messageCount, 0);
             }
+        }
+        
+        public void Dispose()
+        {
+            _ldClient?.Dispose();
         }
     }    
 }
